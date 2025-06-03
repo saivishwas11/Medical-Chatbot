ac5bc2a3714480a37802c47ed1f1d31aff13a3eb
Sai Vishwas Aluvala <saivishwas2003@gmail.com>
2025-06-04 03:37:17 +0530

    done folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative AI Project',
+    version='0.1.0',
+    author='Sai Vishwas Aluvala',
+    author_email='saivishwas2003@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
